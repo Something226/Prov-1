76639f8e97f3d541e77125b36d2ff684dac81e1d
Axel Lieback <axel.lieback@learnet.se>
2020-10-14 14:01:00 +0200

    First

---
+++ Prov1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ship Sinking Time!\nThere is an enemy ship approaching in the far distance, guess the range and see if you can make a hit!");
+
+            Console.ReadLine();
+        }
+    }
+}
